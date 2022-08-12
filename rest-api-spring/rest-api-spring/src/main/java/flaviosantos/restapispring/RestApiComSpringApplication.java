39328e664833706b185a776782ea2db481ca2476
Flavio Santos <flavio.pasantos@outlook.com>
2022-08-12 00:43:33 -0300

    stc: create a project

---
+++ rest-api-spring/rest-api-spring/src/main/java/flaviosantos/restapispring/RestApiComSpringApplication.java
@@ -0,0 +1,13 @@
+package flaviosantos.restapispring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiComSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiComSpringApplication.class, args);
+	}
+
+}
---
+++ rest-api-spring/rest-api-spring/src/test/java/flaviosantos/restapispring/RestApiComSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package flaviosantos.restapispring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiComSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
